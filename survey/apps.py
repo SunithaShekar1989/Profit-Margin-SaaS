17be024e24b555f553419e647ec74765f4cfb235
sunithashekar1989 <onenextgenautomation@gmail.com>
2025-12-25 20:55:29 +0530

    fix SurveyConfig registration

--- survey/apps.py
+++ survey/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 class SurveyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'survey'
+    verbose_name = "Survey"
 
     def ready(self):
         import survey.signals
